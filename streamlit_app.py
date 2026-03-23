760ef72087f95b4d025891b2ada3630a54accbf2
Guillaume Costeseque <30239259+gcostese@users.noreply.github.com>
2026-03-23 15:26:51 +0100

    retouches format

--- streamlit_app.py
+++ streamlit_app.py
@@ -227,7 +227,7 @@ with tab1:
     fig_total.add_trace(go.Scatter(
         x=df_stats['datetime'], y=df_stats['Total'],
         name="Total Personnes Transportées",
-        line=dict(color='#2C3E50', width=3),
+        line=dict(color='#2C3E50', width=1.5),
         mode='lines'
     ))
     fig_total.update_layout(
